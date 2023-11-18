9d1f95b5f9dee68e0b81d7ac1062883933332370
shapeshkanth <shapeshkanth@gmail.com>
2023-11-18 22:30:31 +0530

    create div

--- loginpage.php
+++ loginpage.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loginpage</title>
+    <style type="text/css">
+        div{
+            div{
+            position: absolute;
+            top: 55%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-style: solid;
+            border-color: black;
+            width: 400px;
+            height: 500px;
+            border-radius: 25px;
+
+        }
+        }
+    </style>
 </head>
 <body>
-    
+    <div>
+
+    </div>
 </body>
 </html>
