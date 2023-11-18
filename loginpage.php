bfc0620c8d54bb0decf2badb6cbbf89d87d31fd1
shapeshkanth <shapeshkanth@gmail.com>
2023-11-18 22:54:19 +0530

    username & password

--- loginpage.php
+++ loginpage.php
@@ -16,11 +16,25 @@
             height: 500px;
             border-radius: 25px;
         }
+        h1{
+            text-align: center;
+            color: black;
+        }
+        h3{
+            color: black;
+        }
+        .box{
+            width: 250px;
+            height: 35px;
+            border-radius: 20px;
+        }
     </style>
 </head>
 <body>
     <div>
-
+    <h1>login</h1><br>
+    <h3>&nbsp&nbspUserName :<input type="text" class  ="box"  placeholder="&nbsp &nbsp  &nbsp  &nbsp   UserName..."></h3>
+    <h3>&nbsp&nbspPassword :&nbsp<input type="password" class="box" placeholder="  &nbsp &nbsp  &nbsp  &nbsp Password...."></h3>
     </div>
 </body>
 </html>
