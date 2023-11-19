0bdb407faedb82ea58343d5a37b4bc392e00ae5a
shapeshkanth <shapeshkanth@gmail.com>
2023-11-19 22:11:52 +0530

    create div

--- signinpage.php
+++ signinpage.php
@@ -9,6 +9,15 @@
     </style>
 </head>
 <body>
+<div >
+           <h1>Signin</h1><br>
+           <h3>UserName :<input type="text" class  ="box"  placeholder="&nbsp &nbsp  &nbsp  &nbsp   UserName..."></h3>
+           <h3>Email :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" class  ="box"  placeholder="&nbsp &nbsp  &nbsp  &nbsp   email..."></h3>
+           <h3>Create Password :&nbsp&nbsp&nbsp&nbsp<input type="password" class="box2" placeholder="  &nbsp &nbsp  &nbsp  &nbsp        Password...."></h3>
+           <h3>Conform Password :<input type="password" class="box2" placeholder="  &nbsp &nbsp  &nbsp  &nbsp        Password...."></h3>
+
     
+    <button type="submit">signup</button>     
+    </div>
 </body>
 </html>
