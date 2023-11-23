7a55de1a84c8186bf7f7a36f92f356b2b014254f
shapeshkanth <shapeshkanth@gmail.com>
2023-11-23 21:31:31 +0530

    update

---
+++ database/sqlconnect.php
@@ -0,0 +1,13 @@
+<?
+define('HOST','localhost');
+define('USERNAME','root');
+define('PWD','');
+define('DB','UOV');
+$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+if($connection){
+    echo "Database connected";
+}
+else{
+    die("connection.error".mysqli_connect_error());
+}
+?>
