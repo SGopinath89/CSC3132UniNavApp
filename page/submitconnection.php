392ff6cb364ba3ff84fa9eb9d4cb36a45317fa2c
shapeshkanth <shapeshkanth@gmail.com>
2023-11-30 01:45:44 +0530

    update

--- page/submitconnection.php
+++ page/submitconnection.php
@@ -1,3 +1,18 @@
 <?php
-
+    include("loginpageconnect.php");
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $query = "select * from admindetails where Uname = '$username' and createpwd = '$password'";
+    $result = mysqli_query($conn,$query);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+    if($count==1){
+        header("Location:welcome.php");
+    }
+    else{
+        echo "<script>
+        window.location.href = 'loginpage.php';
+        alert('login failed invalid username or password!!!')
+        </script>";
+    }
 ?>
