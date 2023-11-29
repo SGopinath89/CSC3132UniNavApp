41e97c25fd8e259e1c127c038e05f969884ec461
shapeshkanth <shapeshkanth@gmail.com>
2023-11-29 23:30:58 +0530

    update

--- page/mark.php
+++ page/mark.php
@@ -4,17 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .icon {
+            position: absolute;
+            width: 30px;
+            height: 30px;
+            background-image: url('LocationIcon.png'); 
+            background-size: cover;
+        }
+        </style>
     <script>
-   document.addEventListener("click", function(e) {
+       document.addEventListener('DOMContentLoaded',function(){
+   document.addEventListener("click", function(e) 
+{
     
     var icon = document.createElement("div");
-    icon.className = "emoji";
-    icon.innerHTML = "";
-    icon.style.left = e.clientX + "px";
-    icon.style.top = e.clientY  + "px";
-
-   
+    icon.className = 'icon';
+    var x = e.clientX;
+    var y = e.clientY;
+    icon.style.left = x + "px";
+    icon.style.top = y + "px"; 
     document.body.appendChild(icon);
+});
+window.addEventListener('beforeunload',function()
+{
+    var iicons =document.getElementByClassName('icon');
+    while (icons.length > 0)
+    {
+        icons[0].parentNode.removeChild(icons[0])
+    }
+ });
 });
     </script>
 </head>
