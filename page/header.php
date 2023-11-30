a740418da2c57e7e3a4d1cab0a639630ebc7dc12
sanjee1999 <dotsmartsanjee@gmail.com>
2023-11-30 23:34:19 +0530

    homepage

---
+++ page/header.php
@@ -0,0 +1,19 @@
+<?php
+<div style="background-color:rgba(203, 219, 192, 0.748);">
+<img src="page/resource/uovlogoname.png" width="50%">       
+</div>
+
+<div style="background-color: rgb(63, 179, 0);height: 8px;">
+
+</div>
+<div style="background-color: rgb(32, 96, 32);height: 40px;">
+<table  style="font-size:30px ;
+            color:white;
+            font-family: Barlow medium;" cellpadding="1vw">
+<tr>
+    <td><a href="homepage.php">Home</a></td>
+    <td><a href="page/loginpage.php">Login</a></td>
+</tr>
+</table>
+</div>
+?>
