4660d300a2ac86e91475217667226dfcdf0e9d29
sanjee1999 <dotsmartsanjee@gmail.com>
2023-12-14 22:37:46 +0530

    create js mouseoverevent

--- page/script/pointer.js
+++ page/script/pointer.js
@@ -4,3 +4,20 @@ function showCoords(event) {
     let text = "X coords: " + x + ", Y coords: " + y;
     document.getElementById("demo").innerHTML = text;
   }
+
+  ////MouseOverEvent
+const locationTags = document.querySelectorAll('.location-tag');
+
+locationTags.forEach((tag) => {
+  tag.addEventListener('mouseover', () => {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.innerHTML = tag.dataset.title;
+
+    document.body.appendChild(popup);
+
+    // Position the popup near the hovered tag
+    popup.style.top = `${tag.offsetTop + tag.offsetHeight}px`;
+    popup.style.left = `${tag.offsetLeft}px`;
+  });
+});
