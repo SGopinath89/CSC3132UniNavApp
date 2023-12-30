169ecc211515502647b7d6f2c8bbac5bc516a2be
shapeshkanth <shapeshkanth@gmail.com>
2023-12-30 20:17:12 +0530

    create function updatetable,dupid,emptycheck

---
+++ page/connection/function.php
@@ -0,0 +1,46 @@
+<?php
+function updatetable($conn,$query)
+{
+		$result = mysqli_query($conn,$query);
+		if($result)
+		{
+			echo "succesful update<br>";
+		}
+		else{
+			die("error".mysqli_error($conn));
+		}
+
+}
+function emptycheck($value,$msg)
+{
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+	if(empty($_POST[$value]))
+	{
+		echo "<p>$msg</p>";
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+}
+function dupId($value,$conn)
+{
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		$query="SELECT * FROM student WHERE id='$_POST[$value]';";
+    $count=counttable($conn,$query);
+    if($count>0)
+    {
+    	echo "<p style='color:red '>$value this id already exists <p>";
+    	return true;
+    }
+    else
+    {
+    	return false;
+    }
+    }
+}
+?>
