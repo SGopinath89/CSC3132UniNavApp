e3e07450952232a708522e9b1c3036f607942e27
shapeshkanth <shapeshkanth@gmail.com>
2023-12-30 22:59:16 +0530

    delete draw in table

---
+++ page/deleteDetails.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>ID:</td>
+		    <td><input type="number" name="id"></td>
+	    </tr>
+	    <tr>
+	    	<td><input type="submit" value="delete"></td>
+	    </tr>
+	</table>
+</form>
+<?php
+  require_once'connection.php';
+  require_once'funct.php';
+if($_SERVER['REQUIES_METHOD'] == 'POST')
+{
+	$ID=trim($_POST['id']);
+	$query="DELETE FROM mar_img WHERE id = $ID;";
+    updatetable($connection,$query);
+}
+?>
+</body>
+</html>
