2a6fe7b24c89b10f2f4ce1c3de184968e98be435
sanjee1999 <dotsmartsanjee@gmail.com>
2024-01-11 17:12:03 +0530

    update

--- page/script/xycatcher.js
+++ page/script/xycatcher.js
@@ -13,3 +13,22 @@ function handleClick(event) {
     console.log('Percentage Coordinates - X:', percentageX.toFixed(2), 'Y:', y.toFixed(2));
   
 }
+function showCoords(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    
+    let x_l=x-237;
+    let y_t=y-237;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Display the PHP response (if any)
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("POST", "addplace.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("x=" + (x_l) + "&y=" + (y_t));
+}
